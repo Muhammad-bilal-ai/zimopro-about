19f47ed22a2807dee9aa2fee815ffe47d788987b
Muhammad Bilal <withbilal777@gmail.com>
2024-07-01 19:00:12 +0500

    adding diffrent screen sizes

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function about() {
   return (
-    <div className="relative w-full min-h-screen overflow-x-hidden">
+    <div className="relative w-full h-screen lg:h-[140vh] 2xl:h-[120vh] 3xl:h-[110vh] overflow-hidden">
       <Image
         src="/2023-01-19_22-48-19.png"
         alt="background image"
@@ -11,22 +11,22 @@ function about() {
         height={1080}
       />
       <div className="absolute top-[68px] left-[60px] w-[66px] h-[7px] bg-transparent bg-no-repeat">
-        <Image src="/Rectangle 178.png" alt="Icon" width={50} height={50} />
+        <Image src="/Rectangle 178.png" alt="Icon" width={66} height={7} />
       </div>
       <div className="absolute top-[85px] left-[60px] w-[66px] h-[17px] bg-transparent bg-no-repeat">
-        <Image src="/Group 3054.png" alt="Icon" width={50} height={50} />
+        <Image src="/Group 3054.png" alt="Icon" width={66} height={17} />
       </div>
       <div className="absolute top-[111px] left-[60px] w-[66px] h-[7px] bg-transparent bg-no-repeat">
-        <Image src="/Rectangle 178.png" alt="Icon" width={50} height={50} />
+        <Image src="/Rectangle 178.png" alt="Icon" width={66} height={7} />
       </div>
       <div className="absolute top-[68px] left-[165px] w-[118px] h-[50px] bg-transparent bg-no-repeat">
-        <Image src="/Group 3285.png" alt="icon" width={78} height={128} />
+        <Image src="/Group 3285.png" alt="icon" width={118} height={50} />
       </div>
       <div className="absolute top-[62px] left-[245px] w-[61px] h-[50px] bg-transparent bg-no-repeat">
-        <Image src="/Path 7808.png" alt="icon" width={50} height={60} />
+        <Image src="/Path 7808.png" alt="icon" width={61} height={50} />
       </div>
       <div className="absolute top-[104px] left-[380px] w-[80px] h-[15px] bg-transparent bg-no-repeat">
-        <Image src="/Path 27304.png" alt="icon" width={80} height={14} />
+        <Image src="/Path 27304.png" alt="icon" width={80} height={15} />
       </div>
       <div className="absolute top-[68px] left-[858px] w-[63px] h-[50px] bg-transparent bg-no-repeat">
         <Image src="/Path 1.png" alt="icon" width={63} height={50} />

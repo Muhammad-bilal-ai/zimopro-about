1c3055bcdda4ef6bfa4d9ec028920169e87ed67a
Muhammad Bilal <withbilal777@gmail.com>
2024-07-01 11:04:20 +0500

    adding icons, 6 icons added and styled according to orginal xd design

--- pages/about.js
+++ pages/about.js
@@ -17,10 +17,16 @@ function about() {
         <Image src="/Rectangle 178.png" alt="Icon" width={50} height={50} />
       </div>
       <div className="absolute top-[68px] left-[165px] w-[118px] h-[50px] bg-transparent bg-no-repeat">
-        <Image src="/Group 3285.png" alt="icon" width={50} height={118} />
+        <Image src="/Group 3285.png" alt="icon" width={78} height={128} />
       </div>
-      <div className="absolute top-[68px] left-[289px] w-[61px] h-[50px] bg-transparent bg-no-repeat">
+      <div className="absolute top-[62px] left-[245px] w-[61px] h-[50px] bg-transparent bg-no-repeat">
         <Image src="/Path 7808.png" alt="icon" width={50} height={60} />
+      </div>
+      <div className="absolute top-[104px] left-[380px] w-[80px] h-[15px] bg-transparent">
+        <Image src="/Path 27304.png" alt="icon" width={80} height={14} />
+      </div>
+      <div>
+        <Image src="/Path 1.png" alt="icon" width={63} height={50} />
       </div>
     </div>
   );
